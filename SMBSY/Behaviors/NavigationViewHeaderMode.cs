35ae3600c88702e30ed0d127d1c320b9ff7c0985
Alpha-00 <lebibibioihandsomeboy.ghost@gmail.com>
2024-11-16 20:40:02 +0700

    full update

--- SMBSY/Behaviors/NavigationViewHeaderMode.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SMBSY.Behaviors;
-
-public enum NavigationViewHeaderMode
-{
-    Always,
-    Never,
-    Minimal
-}
--- SMBSY/Contracts/ViewModels/INavigationAware.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SMBSY.Contracts.ViewModels;
-
-public interface INavigationAware
-{
-    void OnNavigatedTo(object parameter);
-
-    void OnNavigatedFrom();
-}
