9466cc75ee6b75561c2208860ddd728b8c88c09b
Alpha-00 <lebibibioihandsomeboy.ghost@gmail.com>
2024-11-16 20:59:46 +0700

    fix: minor form update

--- MMBS/Model/post_form/Mod977/PostForm240603_Mod977.cs
+++ MMBS/Model/post_form/Mod977/PostForm240603_Mod977.cs
@@ -262,7 +262,7 @@ namespace MMBS.Model.PostForm
                 else
                 {
                     var creditString = preview == "OfflineMods.Net" ? "乡HULҜ™" : data.credit.now.GetToUse();
-                    var vipCheck = new List<String>() { "乡HULҜ™", "Apksmodᵗᵘᵇᵒᵗᵒᶜᵈᵒ" };
+                    var vipCheck = new List<String>() { "乡HULҜ™", "Apksmodᵗᵘᵇᵒᵗᵒᶜᵈᵒ", "ApksModᵗᵘᵇᵒᵗᵒᶜᵈᵒ" };
                     try
                     {
                         var vipFile = System.IO.File.ReadAllText(Class1.GetToken("creditVipFile"));
@@ -427,7 +427,7 @@ namespace MMBS.Model.PostForm
         //    ,{ "permReq", "<li>Needs Special Permission: " + "$$$.extpermReqHtml$$$$" + "</li>" + "\n" }
         //};
 
-        public String infoNotifyExtPermMessage => "<span>Để có được các tính năng MOD, bạn phải</span><span style=\"color: red;\"> <a href=\"https://www.mod977.top/2024/11/huong-dan-cap-quyen-truy-cap-bo-nho-ngoai.html\" rel=\"nofollow\" target=\"_blank\"><span><b>Cấp quyền truy cập bộ nhớ ngoài</b></span></a></span>";
+        public String infoNotifyExtPermMessage => "<span>Để có được các tính năng MOD, bạn phải</span><span> <a href=\"https://www.mod977.top/2024/11/huong-dan-cap-quyen-truy-cap-bo-nho-ngoai.html\" rel=\"nofollow\" target=\"_blank\"><span><b>Cấp quyền truy cập bộ nhớ ngoài</b></span></a></span>";
 
         /// <summary>
         /// 
