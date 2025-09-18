a44c2bd6a8c09a6a13b78edf618e59dd1a5cc402
Alpha-00 <lebibibioihandsomeboy.ghost@gmail.com>
2025-09-18 10:18:32 +0700

    fix: test match definition

--- coretestDownloadLink1/browserless_test.cs
+++ coretestDownloadLink1/browserless_test.cs
@@ -26,7 +26,7 @@ namespace coretestDownloadLink1
         public void modsfireTests()
         {
             var browserless = new MMBS.Service.Browserless();
-            browserless.init();
+            //browserless.init();
             var task = browserless.fetch("https://modsfire.com/oCa4r9g72J3M864");
             task.Wait();
             var result = task.Result;
@@ -37,7 +37,7 @@ namespace coretestDownloadLink1
         public void cloudflareTests()
         {
             var browserless = new MMBS.Service.Browserless();
-            browserless.init();
+            //browserless.init();
             var task = browserless.fetchWithCloudflareBypass("https://apkpure.com/vn/plants-vs-zombies/com.ea.game.pvzfree_row");
             task.Wait();
             var result = task.Result;
